3315dc82093a309252c75ab74e1d57c328dc455f
OmamahJunaid <142996131+OmamahJunaid@users.noreply.github.com>
2023-10-17 23:32:51 +0500

    add all files

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 
-var hour = 0
 var min = 0
 var sec = 0
+var milisec = 0
+var milisecheading= document.getElementById("milisec")
 var secheading= document.getElementById("sec")
 var minheading= document.getElementById("min")
-var hourheading= document.getElementById("hours")
 var interval ;
 function time(){
-    sec++
-    secheading.innerHTML=sec
-    if(sec>=60) {
-        min++
-        minheading.innerHTML=min
-        sec = 0
+    milisec++
+    milisecheading.innerHTML=milisec
+    if(milisec>=100) {
+        sec++
+        secheading.innerHTML=sec
+        milisec = 0
     } 
+    else if(sec>=60){
+        min++
+        minheading.innerHTML=min;
+        sec=0;
+
+    }
 }
 function str(){
-interval= setInterval(time,1000)
+interval= setInterval(time,10)
+document.getElementById("button").disabled = true;
 }
 function stop(){
     clearInterval(interval)
+    document.getElementById("button").disabled = false;
 }
 function reset(){
-    var hour = 0
+    
 var min = 0
 var sec = 0
+var milisec = 0
+milisecheading.innerHTML=milisec
 secheading.innerHTML=sec
 minheading.innerHTML=min
-hourheading.innerHTML=hour
+
 stop()
 }
